ad34b02b041ce4ea39be13ce10859a230af25829
shivam45783 <shivamrajan01@gmail.com>
2025-07-19 14:58:36 +0530

    fixing some features

--- admin/src/components/Navbar/Navbar.jsx
+++ admin/src/components/Navbar/Navbar.jsx
@@ -2,14 +2,20 @@ import React from "react";
 import "./Navbar.css";
 import { assets } from "../../assets/assets";
 const Navbar = () => {
+  const Logout = () => {
+    localStorage.removeItem("token");
+    window.location.reload();
+  };
   return (
     <div className="navbar">
       <div className="logo_admin ">
         <img src={assets.logo} alt="logo" className="logo" />
         <h1>Admin</h1>
       </div>
-
-      <img src={assets.profile_image} alt="" className="profile" />
+      <div className="nav-right">
+        <h1 onClick={Logout}>Logout</h1>
+        <img src={assets.profile_image} alt="" className="profile" />
+      </div>
     </div>
   );
 };
